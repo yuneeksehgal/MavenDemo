46695316fbd1473cb9226460385b54c10c742544
yuneeksehgal <yuneeksehgal@gmail.com>
2020-03-24 18:01:26 +0530

    This is commit from IST time zone guy

--- src/test/java/yuneeksolutions/RESTAPITest.java
+++ src/test/java/yuneeksolutions/RESTAPITest.java
@@ -5,10 +5,19 @@ import org.testng.annotations.Test;
 public class RESTAPITest {
 	
 	@Test
-	public void createJIRA()
+	public void postJIRA()
 	{
 		
-		System.out.println("createJIRA");
+		System.out.println("postJIRA");
+		System.out.println("postJIRA2");
+		System.out.println("postJIRA3");
+	}
+	
+	@Test
+	public void deleteTwitter()
+	{
+		
+		System.out.println("deleteTwitter");
 	}
 
 }
